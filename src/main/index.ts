6ae076bb5dc30a68ab0b0bd5e9e1504cbb4fa95f
Sergio-Pedretti <sergiopedretti07@gmail.com>
2023-07-30 11:52:15 -0300

    refactor: move puppeteer to its own file

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,25 +1,5 @@
-import puppeteer, { type Browser, type Page } from 'puppeteer'
-
-export class PuppeteerController {
-  async scrapData (): Promise<any> {
-    const page = await this.initialize()
-    await page.goto('https://www.linkedin.com/')
-    await page.waitForNavigation()
-    await page.setViewport({ width: 1080, height: 1024 })
-    await page.type('#session_key', 'huhiqhjzqldrmeiedo@cazlv.com')
-    await page.type('#session_password', 'pwUKZ33w3cLGrJv')
-    await page.click('.btn-md.btn-primary')
-  }
-
-  private async initialize (): Promise<Page> {
-    const browser = await this.createBrowser()
-    return await browser.newPage()
-  }
-
-  private async createBrowser (): Promise<Browser> {
-    return await puppeteer.launch({ headless: false })
-  }
-}
+import './config/module-alias'
+import { PuppeteerController } from '@/infra/controller/puppeteer-controller'
 
 const puppet = new PuppeteerController()
 
